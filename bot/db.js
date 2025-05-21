b60d4fda27e80d532757121e3e43b7dcb2b2ebcf
Pavel Zagainov <pavel.zagainov@1win.pro>
2025-05-22 00:16:59 +0300

    Продолжаем пляски

--- bot/db.js
+++ bot/db.js
@@ -1,7 +1,8 @@
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGO_URI || "mongodb://localhost:27017";
+const uri = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@mongo:27017/biblebot?authSource=admin`;
 const client = new MongoClient(uri);
+
 export const db = client.db("biblebot");
 export const users = db.collection("users");
 
